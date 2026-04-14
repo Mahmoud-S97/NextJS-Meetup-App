c9e9ff0be0254d54a3a9d3186fa11870d7aa9b41
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2026-04-14 20:28:42 +0100

    First commit, uploading files

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col w-full min-h-screen items-center justify-center">
+      <h1>This is Home Page!</h1>
+    </main>
+  );
+}
