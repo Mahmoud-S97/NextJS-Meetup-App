2c49e55c77f0ba57a083f8622c05808d066a0a1c
Mahmoud Saleh <mahmoudsaleh@MACBOOKPRO-5870.station>
2026-04-25 01:24:40 +0100

    Feat(Dark-Mode): Added dark-mode ThemeToggle for the whole app

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,37 +1,43 @@
 "use client";
-import React, { JSX, useState } from "react";
+import React, { JSX, useEffect, useState } from "react";
 import Logo from "../globals/logo";
 import Link from "next/link";
 import { classes } from "@/utils/classes";
 import { usePathname } from "next/navigation";
 import Icon from "../globals/app-icon";
+import ThemeToggle from "../ui/buttons/toggle-theme-button";
 
 const Header = (props: any): JSX.Element => {
+  
   const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false);
 
   const toggleMenu = () => setShowMobileMenu((prevValue) => !prevValue);
 
   const pathName = usePathname();
 
+  useEffect(() => {
+    localStorage.setItem("theme", "system");
+  }, []);
+
   return (
     <header className="w-full min-h-18 sm:h-18 bg-(--primary)">
       <nav className="w-full h-[100%] relative flex flex-col p-4 sm:px-20 sm:flex-row items-start sm:items-center justify-center sm:justify-between">
         <Link
           href="/"
-          className="w-16 h-16 sm:w-20 sm:h-20 flex items-center justify-center"
+          className="w-16 h-16 sm:w-20 sm:h-20 flex items-center"
           onClick={showMobileMenu ? toggleMenu : () => {}}
         >
           <Logo fillColor="#FFFFFF" width="100%" height="100%" />
         </Link>
         <ul
           className={classes(
-            "hidden w-full sm:w-auto sm:flex sm:flex-row gap-2 sm:gap-6 p-4 sm:p-0 ease-in-out duration-300",
+            "hidden w-full sm:w-auto sm:flex sm:flex-row items-center justify-center gap-2 sm:gap-6 p-4 sm:p-0 ease-in-out duration-300",
             { "flex flex-col": showMobileMenu },
           )}
         >
           <li
             className={classes(
-              "w-full sm:w-auto text-gray-300 relative inline-block hover:text-gray-50 focus:text-gray-50 text-md font-semibold ease duration-200",
+              "w-full sm:w-auto text-gray-300 relative p-2 inline-block hover:text-gray-50 focus:text-gray-50 text-md font-semibold ease duration-200",
               { "text-gray-50 text-shadow-lg": pathName === "/" },
             )}
           >
@@ -45,7 +51,7 @@ const Header = (props: any): JSX.Element => {
           </li>
           <li
             className={classes(
-              "w-full sm:w-auto text-gray-300 inline-block hover:text-gray-50 focus:text-gray-50 text-md font-semibold ease duration-200",
+              "w-full sm:w-auto text-gray-300 p-2 inline-block hover:text-gray-50 focus:text-gray-50 text-md font-semibold ease duration-200",
               { "text-gray-50 text-shadow-lg": pathName.startsWith("/meetups/new") },
             )}
           >
@@ -56,6 +62,9 @@ const Header = (props: any): JSX.Element => {
             >
               Add New Meetup
             </Link>
+          </li>
+          <li className="w-full sm:w-auto text-gray-300 p-2 inline-block hover:text-gray-50 focus:text-gray-50 text-md font-semibold ease duration-200">
+            <ThemeToggle />
           </li>
         </ul>
         <button
---
+++ src/components/ui/buttons/toggle-theme-button.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { JSX, useEffect, useState } from "react";
+
+const ThemeToggle = (): JSX.Element => {
+  const [dark, setDark] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log("Local-Storage: ", localStorage.getItem("theme"));
+
+    const saved = localStorage.getItem("theme");
+    let isDark = saved === "dark";
+
+    if (saved === "system") {
+      isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+
+    setDark(isDark);
+    document.documentElement.classList.toggle("dark", isDark);
+  }, []);
+
+  const toggleTheme = (): void => {
+    const newTheme = !dark;
+
+    setDark(newTheme);
+    document.documentElement.classList.toggle("dark", newTheme);
+    localStorage.setItem("theme", newTheme ? "dark" : "light");
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className="cursor-pointer bg-black/20 rounded-full px-2 py-1"
+    >
+      {dark ? "🌙 Dark" : "☀️ Light"}
+    </button>
+  );
+};
+
+export default ThemeToggle;
